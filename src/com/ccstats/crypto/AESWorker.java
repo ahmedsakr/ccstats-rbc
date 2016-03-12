9cf05a86151e44701189ab04eb8debde41011a8a
ahmed sakr <ahmed.sakr@carleton.ca>
2016-03-11 20:35:27 -0500

    Great simplification and enhancement of the encryption process

--- src/com/ccstats/crypto/AESWorker.java
+++ src/com/ccstats/crypto/AESWorker.java
@@ -37,8 +37,6 @@ import java.security.spec.InvalidParameterSpecException;
  */
 public class AESWorker {
 
-    private byte[] salt, ivBytes;
-
 
     /**
      * Through the power of the advanced encryption standard, a plaintext will be encrypted with a parameter-specified
@@ -70,7 +68,7 @@ public class AESWorker {
         password = hash(new String(password).getBytes(StandardCharsets.UTF_8));
 
         SecureRandom random = new SecureRandom();
-        salt = new byte[20];
+        byte[] salt = new byte[20];
         random.nextBytes(salt);
 
         Cipher cipher = Cipher.getInstance("AES/CBC/PKCS5Padding");
@@ -89,10 +87,10 @@ public class AESWorker {
         cipher.init(Cipher.ENCRYPT_MODE, keySpec);
 
         AlgorithmParameters ap = cipher.getParameters();
-        ivBytes = ap.getParameterSpec(IvParameterSpec.class).getIV();
+        byte[] ivBytes = ap.getParameterSpec(IvParameterSpec.class).getIV();
 
         byte[] result = cipher.doFinal(text);
-        return Hex.encodeHex(result);
+        return Hex.encodeHex(mergeByteArrays(ivBytes, result, salt));
     }
 
 
@@ -168,13 +166,10 @@ public class AESWorker {
 
 
     /**
-     * Decrypting text that is encrypted by the advanced encryption standard. The Salt and Iv bytes are critical
-     * for decryption of the text.
+     * Decrypting text that is encrypted by the advanced encryption standard.
      *
      * @param password The char array containing of the plaintext password
-     * @param encryptedText The Encrypted text to be targeted and decrypted.
-     * @param salt The salt bytes used to encrypt the text.
-     * @param ivBytes The Iv Bytes produced and used in the encryption process.
+     * @param encryptedBlock The Encrypted text to be targeted and decrypted.
      * @param keyLength The AES Key length in bits.
      *
      * @return The decrypted byte array of the encrypted text.
@@ -187,15 +182,30 @@ public class AESWorker {
      * @throws IllegalBlockSizeException
      * @throws InvalidAlgorithmParameterException
      * @throws DecoderException
-     *
-     * @see this#getLatestIvAutoDestroy()
-     * @see this#getLatestSaltAutoDestroy()
      */
-    public byte[] decrypt(char[] password, char[] encryptedText, byte[] salt, byte[] ivBytes, int keyLength) throws
+    public byte[] decrypt(char[] password, char[] encryptedBlock, int keyLength) throws
             NoSuchAlgorithmException, NoSuchPaddingException, InvalidKeySpecException, InvalidKeyException,
             BadPaddingException, IllegalBlockSizeException, InvalidAlgorithmParameterException, DecoderException {
 
         password = hash(new String(password).getBytes(StandardCharsets.UTF_8));
+        byte[] decoded = Hex.decodeHex(encryptedBlock);
+
+        /**
+         * The decoded block has 3 main elements smudged together: ivBytes, encryptedText, and the salt.
+         * The ivBytes is of length 16 and the salt is of length 20. Hence, the encryptedText must be the
+         * subtraction of the sum of the other bytes.
+         */
+        byte[] encryptedText = new byte[decoded.length - 36], ivBytes = new byte[16], salt = new byte[20];
+
+        /**
+         * The decoded bytes are ordered in the following form: ivBytes + encryptedText + saltBytes.
+         * Hence, the proceeding is manipulating the decoded byte's order to grab the ivBytes, encryptedText bytes,
+         * and salt bytes and assign them to their correct attributes.
+         */
+        System.arraycopy(decoded, ivBytes.length, encryptedText, 0, encryptedText.length);
+        System.arraycopy(decoded, 0, ivBytes, 0, ivBytes.length);
+        System.arraycopy(decoded, decoded.length - salt.length, salt, 0, salt.length);
+
         Cipher cipher = Cipher.getInstance("AES/CBC/PKCS5Padding");
         int maxKeyLength = Cipher.getMaxAllowedKeyLength("AES");
 
@@ -205,13 +215,13 @@ public class AESWorker {
                     maxKeyLength, maxKeyLength);
         }
 
-        SecretKeyFactory factory = SecretKeyFactory.getInstance("PBKDF2WithHmacSHA1");
+        SecretKeyFactory factory = SecretKeyFactory .getInstance("PBKDF2WithHmacSHA1");
         PBEKeySpec spec = new PBEKeySpec(password, salt, 65536, keyLength);
         SecretKey key = factory.generateSecret(spec);
         SecretKeySpec keySpec = new SecretKeySpec(key.getEncoded(), "AES");
 
         cipher.init(Cipher.DECRYPT_MODE, keySpec, new IvParameterSpec(ivBytes));
-        return cipher.doFinal(Hex.decodeHex(encryptedText));
+        return cipher.doFinal(encryptedText);
     }
 
 
@@ -221,8 +231,6 @@ public class AESWorker {
      *
      * @param password The plaintext password as a String.
      * @param encryptedText The encrypted text as a String.
-     * @param salt The salt bytes used when at the time of encrypting the text.
-     * @param ivBytes The Iv bytes produced by the encryption and needed for decryption
      * @param keyLength The AES Key length in bits.
      *
      * @return The decrypted byte array of the encrypted text.
@@ -236,10 +244,10 @@ public class AESWorker {
      * @throws InvalidKeyException
      * @throws InvalidKeySpecException
      */
-    public byte[] decrypt(String password, String encryptedText, byte[] salt, byte[] ivBytes, int keyLength) throws
+    public byte[] decrypt(String password, String encryptedText, int keyLength) throws
             NoSuchPaddingException, DecoderException, InvalidAlgorithmParameterException, NoSuchAlgorithmException,
             IllegalBlockSizeException, BadPaddingException, InvalidKeyException, InvalidKeySpecException {
-        return decrypt(password.toCharArray(), encryptedText.toCharArray(), salt, ivBytes, keyLength);
+        return decrypt(password.toCharArray(), encryptedText.toCharArray(), keyLength);
     }
 
 
@@ -249,8 +257,6 @@ public class AESWorker {
      *
      * @param password The plaintext password as a String.
      * @param encryptedText The encrypted text as a String.
-     * @param salt The salt bytes used when at the time of encrypting the text.
-     * @param ivBytes The Iv bytes produced by the encryption and needed for decryption
      *
      * @return The decrypted byte array of the encrypted text.
      *
@@ -263,76 +269,10 @@ public class AESWorker {
      * @throws InvalidKeyException
      * @throws InvalidKeySpecException
      */
-    public byte[] decrypt(String password, String encryptedText, byte[] salt, byte[] ivBytes) throws
+    public byte[] decrypt(String password, String encryptedText) throws
             NoSuchPaddingException, DecoderException, InvalidAlgorithmParameterException, NoSuchAlgorithmException,
             IllegalBlockSizeException, BadPaddingException, InvalidKeyException, InvalidKeySpecException {
-        return decrypt(password.toCharArray(), encryptedText.toCharArray(), salt, ivBytes, 256);
-    }
-
-
-    /**
-     * An override of the decrypt method with the ability to provide the password and encryptedText as character
-     * arrays, and attempt to extract the latest ivBytes and salt for usage.
-     *
-     * @param password The plaintext password as a char array.
-     * @param encryptedText The encrypted text as a char array.
-     * @param keyLength  The AES Key Length in bits.
-     *
-     * @return The decrypted byte array of the encrypted text.
-     *
-     * @throws NoSuchPaddingException
-     * @throws DecoderException
-     * @throws InvalidAlgorithmParameterException
-     * @throws NoSuchAlgorithmException
-     * @throws IllegalBlockSizeException
-     * @throws BadPaddingException
-     * @throws InvalidKeyException
-     * @throws InvalidKeySpecException
-     */
-    public byte[] decrypt(char[] password, char[] encryptedText, int keyLength) throws NoSuchPaddingException, DecoderException,
-            InvalidAlgorithmParameterException, NoSuchAlgorithmException, IllegalBlockSizeException,
-            BadPaddingException, InvalidKeyException, InvalidKeySpecException {
-        byte[] ivBytes = getLatestIvAutoDestroy();
-        byte[] salt = getLatestSaltAutoDestroy();
-
-        if (ivBytes == null) {
-            System.err.println("Attempted to decrypt text with null Iv Bytes, returning null...");
-            return null;
-        }
-
-        if (salt == null) {
-            System.err.println("Attempted to decrypt text with null salt bytes, returning null...");
-            return null;
-        }
-
-        return decrypt(password, encryptedText, salt, ivBytes, keyLength);
-    }
-
-
-
-    /**
-     * An override of the decrypt method with the ability to provide the password and encryptedText as String objects,
-     * and attempt to extract the latest ivBytes and salt for usage.
-     *
-     * @param password The plaintext password as a char array.
-     * @param encryptedText The encrypted text as a char array.
-     * @param keyLength The AES Key length in bits.
-     *
-     * @return The decrypted byte array of the encrypted text.
-     *
-     * @throws NoSuchPaddingException
-     * @throws DecoderException
-     * @throws InvalidAlgorithmParameterException
-     * @throws NoSuchAlgorithmException
-     * @throws IllegalBlockSizeException
-     * @throws BadPaddingException
-     * @throws InvalidKeyException
-     * @throws InvalidKeySpecException
-     */
-    public byte[] decrypt(String password, String encryptedText, int keyLength) throws NoSuchPaddingException, DecoderException,
-            InvalidAlgorithmParameterException, NoSuchAlgorithmException, IllegalBlockSizeException,
-            BadPaddingException, InvalidKeyException, InvalidKeySpecException {
-        return decrypt(password.toCharArray(), encryptedText.toCharArray(), keyLength);
+        return decrypt(password.toCharArray(), encryptedText.toCharArray(), 256);
     }
 
 
@@ -363,32 +303,6 @@ public class AESWorker {
 
 
     /**
-     * An override of the decrypt method with the ability to provide the password and encryptedText as String
-     * objects, and attempt to extract the latest ivBytes and salt for usage. The Default AES Key length is set to
-     * 256 bits.
-     *
-     * @param password The plaintext password as a String object.
-     * @param encryptedText The encrypted text as a String object.
-     *
-     * @return The decrypted byte array of the encrypted text.
-     *
-     * @throws NoSuchPaddingException
-     * @throws InvalidKeyException
-     * @throws DecoderException
-     * @throws IllegalBlockSizeException
-     * @throws BadPaddingException
-     * @throws NoSuchAlgorithmException
-     * @throws InvalidAlgorithmParameterException
-     * @throws InvalidKeySpecException
-     */
-    public byte[] decrypt(String password, String encryptedText) throws NoSuchPaddingException,
-            InvalidAlgorithmParameterException, DecoderException, IllegalBlockSizeException, BadPaddingException,
-            NoSuchAlgorithmException, InvalidKeyException, InvalidKeySpecException {
-        return decrypt(password.toCharArray(), encryptedText.toCharArray());
-    }
-
-
-    /**
      * Hashes the plain password to provide a more secure experience.
      *
      * @param password the bytes of the plaintext password.
@@ -404,35 +318,21 @@ public class AESWorker {
         return Hex.encodeHex(md.digest());
     }
 
-    /**
-     * Be careful while using this method: The salt auto destroys after one call for security measures. You
-     * should take similar measures and store it somewhere safe and minimize its appearance.
-     *
-     * Moreover, calling the encrypt method directly after a prior call will destroy the previously stored salt.
-     *
-     * @return The Latest salt used to encrypt text, and automatically null the attribute.
-     */
-    public byte[] getLatestSaltAutoDestroy() {
-        byte[] temp = salt;
-        salt = null;
 
-        return temp;
-    }
+    private byte[] mergeByteArrays(byte[]... arrays) {
+        int capacity = 0;
+        for (byte[] arr : arrays) {
+            capacity += arr.length;
+        }
 
+        byte[] result = new byte[capacity];
+        int index = 0;
+        for (byte[] array : arrays) {
+            System.arraycopy(array, 0, result, index, array.length);
+            index += array.length;
+        }
 
-    /**
-     * Be careful while using this method: The Iv bytes auto destroy after one call for security measures. You
-     * should take similar measures and store it somewhere safe and minimize its appearance.
-     *
-     * Moreover, calling the encrypt method directly after a prior call will destroy the previously stored salt.
-     *
-     * @return The Latest Iv bytes used to encrypt text, and automatically null the attribute.
-     */
-    public byte[] getLatestIvAutoDestroy() {
-        byte[] temp = ivBytes;
-        ivBytes = null;
-
-        return temp;
+        return result;
     }
 
 }
