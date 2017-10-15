c6c9bace91e1735eda4e0327105c3667d56fcfd1
ahmed sakr <ahmed.sakr@carleton.ca>
2017-10-15 11:34:08 -0400

    Updated Test.java to read encrypted statements and perform weekly analysis

--- src/com/ccstats/test/Test.java
+++ src/com/ccstats/test/Test.java
@@ -2,11 +2,17 @@ package com.ccstats.test;
 
 import com.ccstats.analysis.TransactionPool;
 import com.ccstats.analysis.worker.TransactionsExtractor;
+import com.ccstats.crypto.io.JSONEncryptedStatement;
 import com.ccstats.data.Statement;
+import com.ccstats.data.Transaction;
 import com.ccstats.input.CreditStatement;
 import com.ccstats.input.exceptions.InvalidStatementPathException;
+import org.json.simple.parser.ParseException;
 
+import javax.crypto.BadPaddingException;
 import java.io.IOException;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 
 
 public class Test {
@@ -14,26 +20,31 @@ public class Test {
 
     public static void main(String[] args) {
         try {
+            if (args.length < 2) {
+                System.err.println("Invalid number of args.");
+                System.exit(1);
+            }
 
-            CreditStatement file = new CreditStatement("/home/ahmed/Downloads/1119-statement.html");
-            CreditStatement file1 = new CreditStatement("/home/ahmed/Downloads/9787-statement.html");
+            JSONEncryptedStatement io = new JSONEncryptedStatement();
+            Statement master = io.read(args[0], args[1]);
 
-            TransactionsExtractor extractor = new TransactionsExtractor(file);
-            Statement master = new Statement(extractor.read());
+            TransactionPool credit = master.getCreditTransactions();
+            String[][] billingDates = {
+                    {"Sep 01, 2017", "Sep 07, 2017"}, {"Sep 08, 2017", "Sep 14, 2017"}, {"Sep 15, 2017", "Sep 21, 2017"},
+                    {"Sep 22, 2017", "Sep 28, 2017"}, {"Sep 29, 2017", "Sep 30, 2017"}, {"Oct 01, 2017", "Oct 07, 2017"},
+                    {"Oct 08, 2017", "Oct 14, 2017"}
+            };
 
-            extractor.setSource(file1);
-            Statement statement1 = new Statement(extractor.read());
-            master.merge(statement1);
-
-            //master.forEach(System.out::println);
-            TransactionPool transactions = master.getCreditTransactions().getTransactionsFrom("Sep 01, 2017", "Oct 01, 2017");
-            transactions.removeTransactionsByKeyword("ROGERS");
-            transactions.removeTransactionsByKeyword("HYDRO");
-            transactions.removeTransactionsByKeyword("Amazon *Marketplce");
-            transactions.forEach(System.out::println);
-            System.out.println(transactions.getBalance());
-            System.out.println(transactions.getAverageDay());
-        } catch (InvalidStatementPathException | IOException e) {
+            TransactionPool transactions;
+            for (String[] dates : billingDates) {
+                System.out.printf("Transactions statistics for %s -> %s:\n---\n", dates[0], dates[1]);
+                transactions = credit.getTransactionsFrom(dates[0], dates[1]);
+                System.out.printf("Balance: $%.2f\n", transactions.getBalance());
+                System.out.printf("Average / Day: $%.2f\n", transactions.getAverageDay());
+                System.out.printf("Standard Deviation: +/- $%.2f\n", transactions.getStandardDeviation());
+                System.out.printf("# of transactions: %d\n---\n", transactions.size());
+            }
+        } catch (IOException | ParseException | BadPaddingException e) {
             e.printStackTrace();
         }
     }
